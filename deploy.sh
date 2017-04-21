adcace5b4fa960d6caaea47c8a9c4460c599ec3f
eneko <eneko@debian-eneko>
2017-04-21 19:07:16 +0200

    Fixed directory issues

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
+# Get current directory
+wd=$(pwd)
+
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 nvm install stable
 npm install -g bower
@@ -7,13 +10,14 @@ bower install quill
 cd bower_components/quill
 npm install
 npm test
-cd ../../
+cd $wd
 
 # Bootstrap
 bower install bootstrap
 cd bower_components/bootstrap
 npm install
 npm test
+cd $wd
 
 # Slick
 bower install kenwheeler/slick
